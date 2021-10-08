62d7335a79f28ef72ca9db8e4dc2e1f6380fb61b
Chandler Cain <chandler@clcain.com>
2021-10-08 13:22:02 -0400

    Use SHALL_DIR

--- install.sh
+++ install.sh
@@ -9,3 +9,8 @@ if ! $(grep -q -F "$shall_groups_dir" ~/.ssh/config)
 then
     echo "Include $shall_groups_dir*" >> ~/.ssh/config
 fi
+
+if [ -z "$SHALL_DIR" ]
+then
+    echo "Please define SHALL_DIR in your .bashrc file."
+fi
